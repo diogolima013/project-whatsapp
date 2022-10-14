6df1cf900477fcf79431c8105914faeea2248f7d
Diogo Lima <diogoslva013@gmail.com>
2022-10-14 10:12:57 -0300

    updating user profile data

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -23,15 +23,33 @@ export class WhatsAppController {
 
         this._firebase.initAuth().then(response=>{
            
-            this._user = new User();
+            this._user = new User(response.user.email);
 
-            let userRef = User.findByEmail(response.user.email);
+            this._user.on('datachange', data=>{
 
-            userRef.set({
-                name: response.user.displayName,
-                email: response.user.email,
-                photo: response.user.photoURL
-            }).then(()=>{
+                document.querySelector('title').innerHTML = data.name + 'WhatsApp Clone';
+
+                this.el.inputNamePanelEditProfile.innerHTML = data.name;
+
+                if(data.photo){
+
+                    let photo = this.el.imgPanelEditProfile;
+                    photo.src = data.photo;
+                    photo.show(); 
+                    this.el.imgDefaultPanelEditProfile.hide();
+
+                   let photo2 = this.el.myPhoto.querySelector('img')
+                   photo2.src = data.photo;
+                   photo2.show(); 
+                }
+
+            });
+
+            this._user.name = response.user.displayName;
+            this._user.email = response.user.email;
+            this._user.photo = response.user.photoURL;
+
+            this._user.save().then(()=>{
 
                 this.el.appContent.css({
                     display:'flex'
@@ -183,7 +201,15 @@ export class WhatsAppController {
 
         this.el.btnSavePanelEditProfile.on('click', e=>{
 
-            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+            this.el.btnSavePanelEditProfile = true;
+
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+
+            this._user.save().then(()=>{
+
+                this.el.btnSavePanelEditProfile = false;
+
+            })
 
         });
 
