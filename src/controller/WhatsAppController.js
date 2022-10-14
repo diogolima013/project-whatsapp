4bc30883ef9aa29f0e3f019869b821347eee310c
Diogo Lima <diogoslva013@gmail.com>
2022-10-14 08:36:11 -0300

    configuring firebase

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,16 +2,37 @@ import {Format} from './../util/Format'
 import {CameraController} from './CameraController';
 import { MicrophoneController } from './MicrophoneController';
 import {DocumentPreviewController} from './DocumentPreviewController';
+import { Firebase } from './../util/Firebase';
 
 export class WhatsAppController {
 
     constructor(){
 
         console.log('WhatsAppController ok')
-
+        
+        this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
+
+    }
+
+    initAuth(){
+
+        this._firebase.initAuth().then(response=>{
+           
+            this._user = response.user;
+
+            this.el.appContent.css({
+                display:'flex'
+
+            });
+
+        })
+        .catch(err=>{
+            console.log(err);
+        });
 
     }
 
---
+++ src/util/Firebase.js
@@ -0,0 +1,73 @@
+const firebase = require('firebase');
+require('firebase/firestore')
+
+
+export class Firebase {
+
+    constructor(){
+
+        this._firebaseConfig = {
+            apiKey: "AIzaSyDGimuyqUIjd2Wzg2ivnWMu1zLDDCurp9o",
+            authDomain: "projeto-whatsapp-1b410.firebaseapp.com",
+            projectId: "projeto-whatsapp-1b410",
+            storageBucket: "projeto-whatsapp-1b410.appspot.com",
+            messagingSenderId: "385529674004",
+            appId: "1:385529674004:web:2361b421dd96f3c502aa01",
+            measurementId: "G-J5NZGNS8ZB"
+        }
+
+        this.init();
+
+    }
+
+    init(){
+    
+        if(!this._initialized){
+            firebase.initializeApp(this._firebaseConfig);
+            firebase.firestore().settings({});
+
+            this._initialized = true;
+
+        }
+    
+    }
+
+    static db(){
+
+        return firebase.firestore();
+
+    }
+
+    static hd(){
+
+        return firebase.storage();
+
+    }
+
+    initAuth(){
+
+        return new Promise((s, f)=>{
+
+            let provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+            .then(result=>{
+
+                let token = result.credential.accessToken;
+                let user = result.user;
+
+                s({
+                    user, 
+                    token
+                });
+
+            })
+            .catch(err=>{
+                f(err);
+            });//aqui ele abre uma janela, perguntando de qual forma deseja acessar.
+
+        });
+
+    }
+
+}
