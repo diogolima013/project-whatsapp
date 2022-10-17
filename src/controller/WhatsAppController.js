54c983ce40a8c7dcb3a8a44d0f9748a0f5cafb27
Diogo Lima <diogoslva013@gmail.com>
2022-10-17 17:54:47 -0300

    Sending a document as an attachment

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ import { Firebase } from './../util/Firebase';
 import { User } from '../model/User';
 import { Chat } from '../model/Chat';
 import { Message } from '../model/Message';
+import { Base64 } from "../util/base64";
 
 export class WhatsAppController {
 
@@ -216,7 +217,16 @@ export class WhatsAppController {
 
                 this.el.panelMessagesContainer.appendChild(view);
 
-                } else if(me) {
+                } else {
+                    
+                    let view = message.getViewElement(me);
+
+                    this.el.panelMessagesContainer.querySelector('#_' + data.id).innerHTML = view.innerHTML;
+                    
+                }
+                
+                
+                if(this.el.panelMessagesContainer.querySelector('#_' + data.id) && me) {
 
                     let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id);
 
@@ -522,7 +532,50 @@ export class WhatsAppController {
 
         this.el.btnSendPicture.on('click', e=>{
 
-            console.log(this.el.pictureCamera.src);
+            this.el.btnSendPicture.disable = true;
+
+            let regex = /^data:(.+);base64,(.*)$/; //expressão
+            let result = this.el.pictureCamera.src.match(regex);
+            let mimeType =  result[1];
+            let ext = mimeType.split('/')[1];
+            let filename = `camera${Date.now()}.${ext}`;
+
+            let picture = new Image();
+            picture.src = this.el.pictureCamera.src;
+            picture.onload = e =>{
+
+                let canvas = document.createElement('canvas')
+                let context = canvas.getContext('2d');
+
+                canvas.width = picture.width;
+                canvas.height = picture.height;
+
+                context.translate(picture.width, 0);
+                context.scale(-1, 1);
+
+                context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+                fetch(canvas.toDataURL(mimeType))
+                .then(res =>{return res.arrayBuffer(); })
+                .then(buffer =>{return new File([buffer], filename, {type: mimeType}) ;})
+                .then(file=>{
+
+                    Message.sendImage(this._contactActive.chatId, this._user.name, file);
+
+                    this.el.btnSendPicture.disable = false;
+
+                    this.closeAllMainPanel();
+                    this._camera.stop();
+                    this.el.btnReshootPanelCamera.hide();
+                    this.el.pictureCamera.hide();
+                    this.el.videoCamera.show();
+                    this.el.containerSendPicture.hide();
+                    this.el.containerTakePicture.show();
+                    this.el.panelMessagesContainer.show();
+
+                });
+
+            }
 
         });
 
@@ -610,7 +663,28 @@ export class WhatsAppController {
 
         this.el.btnSendDocument.on('click', e=>{
 
-            console.log('send document');
+           let file = this.el.inputDocument.files[0];
+           let base64 = this.el.imgPanelDocumentPreview.src
+
+            if (file.type === 'application/pdf'){
+
+                Base64.toFile(base64).then(filePreview =>{
+
+                    Message.sendDocument(
+                        this._contactActive.chatId, 
+                        this._user.name, file, filePreview, this.el.infoPanelDocumentPreview.innerHTML);
+
+                });
+
+            } else {
+
+                Message.sendDocument(
+                    this._contactActive.chatId, 
+                    this._user.name, file);
+
+            }
+
+            this.el.btnClosePanelDocumentPreview.click();
 
         });
 
--- src/model/Message.js
+++ src/model/Message.js
@@ -2,6 +2,7 @@ import { Firebase } from "../util/Firebase";
 import { Format } from "../util/Format";
 import { Model } from "./Model";
 
+
 export class Message extends Model{
 
     constructor(){
@@ -22,10 +23,30 @@ export class Message extends Model{
 
     get status() {return this._data.status;}
     set status(value) {return this._data.status = value}
+    
+    get preview() {return this._data.preview;}
+    set preview(value) {return this._data.preview = value}
+
+    get info() {return this._data.info;}
+    set info(value) {return this._data.info = value}
+    
+    get fileType() {return this._data.fileType;}
+    set fileType(value) {return this._data.fileType = value}
+
+    get from() {return this._data.from;}
+    set from(value) {return this._data.from = value}
+
+    get size() {return this._data.size;}
+    set size(value) {return this._data.size = value}
+
+    get filename() {return this._data.filename;}
+    set filename(value) {return this._data.filename = value}
 
     getViewElement(me = true){
 
         let div = document.createElement('div');
+
+        div.id = `_${this.id}`
 
         div.className = 'message';
 
@@ -134,13 +155,13 @@ export class Message extends Model{
                 <div class="_3_7SH _1ZPgd ">
                     <div class="_1fnMt _2CORf">
                         <a class="_1vKRe" href="#">
-                            <div class="_2jTyA" style="background-image: url()"></div>
+                            <div class="_2jTyA" style="background-image: url(${this.preview})"></div>
                             <div class="_12xX7">
                                 <div class="_3eW69">
                                     <div class="JdzFp message-file-icon icon-doc-pdf"></div>
                                 </div>
                                 <div class="nxILt">
-                                    <span dir="auto" class="message-filename">Arquivo.pdf</span>
+                                    <span dir="auto" class="message-filename">${this.filename}</span>
                                 </div>
                                 <div class="_17viz">
                                     <span data-icon="audio-download" class="message-file-download">
@@ -158,9 +179,9 @@ export class Message extends Model{
                             </div>
                         </a>
                         <div class="_3cMIj">
-                            <span class="PyPig message-file-info">32 páginas</span>
-                            <span class="PyPig message-file-type">PDF</span>
-                            <span class="PyPig message-file-size">4 MB</span>
+                            <span class="PyPig message-file-info">${this.info}</span>
+                            <span class="PyPig message-file-type">${this.fileType}</span>
+                            <span class="PyPig message-file-size">${this.size}</span>
                         </div>
                         <div class="_3Lj_s">
                             <div class="_1DZAH" role="button">
@@ -170,6 +191,12 @@ export class Message extends Model{
                     </div>
                 </div>
                 `;
+
+            div.on('click', e=>{
+
+                window.open(this.content);
+
+            });
             break;
 
             case 'audio':
@@ -291,37 +318,104 @@ export class Message extends Model{
 
     }
 
-    static sendImage(chatId, from, file){
+    static upload(file, from){
 
-        return new Promise((s, f)=>{
+        return new Promise((s, f )=>{
 
-        let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
+            let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
 
-        uploadTask.on('state_changed', e=>{
+             uploadTask.on('state_changed', e=>{
 
             console.info('upload', e);
 
         }, err =>{
+            
             console.error(err);
+
         }, ()=>{
 
-            uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-                Message.send(
-                    chatId, 
-                    from, 
-                    'image', 
-                    downloadURL  
-            ).then(()=>{
+           uploadTask.snapshot.ref.getDownloadURL().then(downloadURL =>{
 
-                s();
+            s(downloadURL)
+
+           })
+          
+     });
+
+    });    
+
+ }
+
+    static sendDocument(chatId, from, file, filePreview, info){
+
+        Message.send(chatId, from, 'document', '').then(msgRef =>{
+            
+            Message.upload(file, from).then(downloadURL=>{
+
+                let downloadFile = downloadURL;
+
+                    if(filePreview) {
+
+                    Message.upload(filePreview, from).then(downloadURL2 =>{
+
+                    let dowloadPreview = downloadURL2;
+        
+                    msgRef.set({
+                        content: downloadFile,
+                        preview: dowloadPreview,
+                        filename: file.name,
+                        size: file.size,
+                        fileType: file.type,
+                        status: 'sent',
+                        info
+                    }, {
+                        merge : true
+                    });
+
+                });
+                    
+                 } else {
+
+                    msgRef.set({
+                        content: downloadFile,
+                        filename: file.name,
+                        size: file.size,
+                        fileType: file.type,
+                        status: 'sent'
+                    }, {
+                        merge : true
+                    });      
+
+                 }
 
             });
 
         });
+    }
 
-    });
-  
-    })
+
+    static sendImage(chatId, from, file){
+
+        return new Promise((s, f)=>{
+
+            Message.upload(file, from).then(downloadURL=>{
+
+                    Message.send(
+                        chatId, 
+                        from, 
+                        'image', 
+                        downloadURL
+                ).then(()=>{
+
+                    s();
+
+                });
+
+            });
+
+           
+        });
+
 }
 
 
@@ -337,13 +431,15 @@ export class Message extends Model{
             from
         }).then(result=>{
 
-            result.parent.doc(result.id).set({
+            let docRef =  result.parent.doc(result.id);
+
+            docRef.set({
                 status:'sent'
             }, {
                 merge:true
             }).then(()=>{
 
-                s();
+                s(docRef);
 
             });
 
