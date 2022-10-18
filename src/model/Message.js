217014cadad334c1fe601a7e1324d4c23d5835ae
Diogo Lima <diogoslva013@gmail.com>
2022-10-18 15:56:00 -0300

    changing user profile picture

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,5 +1,6 @@
 import { Firebase } from "../util/Firebase";
 import { Format } from "../util/Format";
+import { Upload } from "../util/Upload";
 import { Model } from "./Model";
 
 
@@ -411,29 +412,7 @@ export class Message extends Model{
 
     static upload(file, from){
 
-        return new Promise((s, f )=>{
-
-            let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
-
-             uploadTask.on('state_changed', e=>{
-
-            console.info('upload', e);
-
-        }, err =>{
-            
-            f(err);
-
-        }, ()=>{
-
-           uploadTask.snapshot.ref.getDownloadURL().then(downloadURL =>{
-
-            s(downloadURL)
-
-           })
-          
-     });
-
-    });    
+       return Upload.send(file, from);
 
  }
 
